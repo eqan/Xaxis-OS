febe420b590081d8d920baed9da618d543ecd322
eqan <eqan.ahmad123@gmail.com>
2021-07-16 13:44:15 +0500

    Bug Fixes

--- memusage.sh
+++ memusage.sh
@@ -1,3 +1,3 @@
 # Returns The Memory Used The By $1 Process
-echo `pmap $1 | tail -n 1 | awk '/[0-9]K/{print $2}'` >> /home/winepine/Desktop/oslabs/finalproject/processes_mem
+echo `pmap $1 | tail -n 1 | awk '/[0-9]K/{print $2}'` >> ~/Desktop/Xaxis-OS/processes_mem
 #echo $x >> processes_mem
